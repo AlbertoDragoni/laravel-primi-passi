6e48f27ef6730faaa31ff5cb5211f76a326ee13b
Alberto Dragoni <albertodragoni@yahoo.it>
2020-05-05 16:13:23 +0200

    lavorazione sass

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+  <img class="logo" src="{{asset('img/marchio-sito-test.png')}}" alt="logo">
+  <nav class="header-nav">
+    <ul class="main-menu">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Prodotti</a></li>
+      <li><a href="#">Contatti</a></li>
+    </ul>
+  </nav>
+</header>
