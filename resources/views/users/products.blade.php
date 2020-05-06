b0d7d64bc7555c3de0c4aa0987616971f2c1564d
Alberto Dragoni <albertodragoni@yahoo.it>
2020-05-06 15:34:52 +0200

    database ok

--- resources/views/users/products.blade.php
+++ resources/views/users/products.blade.php
@@ -1,1 +1,20 @@
 @extends('users.layout')
+@section('titolo')
+  Homepage
+@endsection
+@php
+$cards = config('dati');
+
+$lunghe = [];
+$corte = [];
+$cortissime = [];
+foreach ($cards as $key => $card) {
+  if ($card['tipo'] == 'lunga') {
+    $lunghe[$key] = $card;
+  } elseif ($card['tipo'] == 'corta') {
+    $corte[$key] = $card;
+  } elseif ($card['tipo'] == 'cortissima') {
+    $cortissime[$key] = $card;
+  }
+}
+@endphp
