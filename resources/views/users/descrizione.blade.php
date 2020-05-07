5d52bb041c5afec8c086363cc4d87cc10dc4238b
Alberto Dragoni <albertodragoni@yahoo.it>
2020-05-07 17:15:02 +0200

    (2)

--- resources/views/users/descrizione.blade.php
+++ resources/views/users/descrizione.blade.php
@@ -8,12 +8,13 @@
 @endsection
 @section('mainContent')
   @foreach ($cards as $key => $card)
-    <h3><a href="{{route('descrizione.show', $key)}}">{{$card['titolo']}}</a></h3>
-    <div class="descrizione-pasta">
-      <img src="{{$card['src-h']}}" alt="">
-      <img src="{{$card['src-p']}}" alt="">
-      <p>{{$card['descrizione']}}</p>
+    <div class="pasta-card">
+      <h3><a href="{{route('descrizione.show', $key)}}">{{$card['titolo']}}</a></h3>
+      <div class="descrizione-pasta">
+        <img src="{{$card['src-h']}}" alt="">
+        <img src="{{$card['src-p']}}" alt="">
+        <p>{{$card['descrizione']}}</p>
+      </div>
     </div>
-
   @endforeach
 @endsection
--- resources/views/users/pasta.blade.php
+++ resources/views/users/pasta.blade.php
@@ -10,12 +10,12 @@
   Homepage -{{$pasta['titolo']}}
 @endsection
 @section('mainContent')
-
-    <h3>{{$pasta['titolo']}}</h3>
-    <div class="descrizione-pasta">
-      <img src="{{$pasta['src-h']}}" alt="">
-      <img src="{{$pasta['src-p']}}" alt="">
-      <p>{{$pasta['descrizione']}}</p>
+    <div class="pasta-card">
+      <h3>{{$pasta['titolo']}}</h3>
+      <div class="descrizione-pasta">
+        <img src="{{$pasta['src-h']}}" alt="">
+        <img src="{{$pasta['src-p']}}" alt="">
+        <p>{{$pasta['descrizione']}}</p>
+      </div>
     </div>
-
 @endsection
